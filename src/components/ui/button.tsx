42fc413a618cf50127a2579531a7e1344cec0dde
钮国伟 <ngwszsd@qq.com>
2025-11-19 14:13:26 +0800

    chore: 修改构建产物打包器

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -56,7 +56,7 @@ function Button({
         buttonVariants({
           variant,
           size,
-          className: `cursor-pointer ${className}`,
+          className: `cursor-pointer ccccccxxcc2333x111111 ${className}`,
         })
       )}
       {...props}
